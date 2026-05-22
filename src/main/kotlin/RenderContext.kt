91df9e361a52eaac6edd5d33abe2fdb96636205d
Adam Mathlay <mathlayadam@icloud.com>
2026-05-22 12:03:55 +0200

    clean up useless shit, no need for deprecations

--- src/main/kotlin/RenderContext.kt
+++ src/main/kotlin/RenderContext.kt
@@ -3,7 +3,6 @@ package io.cuttlefish
 import kotlin.math.ceil
 
 class RenderContext(override val width: Int, override val height: Int) : Bitmap(width, height) {
-    val scanBuffer = IntArray(2 * height)
 
 
     fun fillTriangle(v1: Vertex, v2: Vertex, v3: Vertex) {
@@ -46,36 +45,6 @@ class RenderContext(override val width: Int, override val height: Int) : Bitmap(
 //        scanTriangle(minY, midY, maxY, whichSide)
 //        fillShape(minY.y.toInt(), maxY.y.toInt())
 
-    }
-
-    @Deprecated("")
-    fun drawScanBuffer(y: Int, xMin: Int, xMax: Int) {
-        scanBuffer[y * 2] = xMin; scanBuffer[y * 2 + 1] = xMax
-    }
-
-    @Deprecated("")
-    fun fillShape(yMin: Int, yMax: Int) {
-        for (i in yMin until yMax) {
-            val xMin = scanBuffer[i * 2]
-            val xMax = scanBuffer[i * 2 + 1]
-
-            for (j in xMin until xMax) {
-                drawPixels(
-                    j, i,
-                    a = 0xFF.toByte(),
-                    b = 0xFF.toByte(),
-                    g = 0xFF.toByte(),
-                    r = 0xFF.toByte(),
-                )
-            }
-        }
-    }
-
-    @Deprecated("")
-    fun scanConvertTriangle(minY: Vertex, midY: Vertex, maxY: Vertex, whichSide: Int) {
-        scanConvertLine(minY, maxY, 0 + whichSide)
-        scanConvertLine(minY, midY, 1 - whichSide)
-        scanConvertLine(midY, maxY, 1 - whichSide)
     }
 
     fun scanTriangle(minY: Vertex, midY: Vertex, maxY: Vertex, whichSide: Boolean) {
@@ -137,28 +106,6 @@ class RenderContext(override val width: Int, override val height: Int) : Bitmap(
                 r = 0xFF.toByte(),
             )
         }
-
-    }
-
-    private fun scanConvertLine(minY: Vertex, maxY: Vertex, whichSide: Int) {
-        val yStart = minY.y
-        val yEnd = maxY.y
-        val xStart = minY.x
-        val xEnd = maxY.x
-
-        val yDistance = yEnd - yStart
-        val xDistance = xEnd - xStart
-
-        if (yDistance <= 0) return
-
-        val xStep: Float = xDistance / yDistance
-        var xCurrent: Float = xStart
-        for (j in yStart.toInt() until yEnd.toInt()) {
-            scanBuffer[j * 2 + whichSide/* min/max side*/] = xCurrent.toInt()
-            xCurrent += xStep
-
-        }
-
 
     }
 }
