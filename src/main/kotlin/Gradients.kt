0f93d01d575699f5c30a4aed08ac558dde349044
Adam Mathlay <mathlayadam@icloud.com>
2026-05-22 14:03:04 +0200

    I PRAY GRADIENTS WORK. IT LOOKS SO BUNS!!!

---
+++ src/main/kotlin/Gradients.kt
@@ -0,0 +1,15 @@
+package io.cuttlefish
+
+class Gradients(miny: Vertex, midy: Vertex, maxy: Vertex) {
+    private val colour = arrayOf(miny.colour, midy.colour, maxy.colour)
+
+    private val oneOverDX =
+        1f / ((miny.pos.x - maxy.pos.x) * (miny.pos.y - maxy.pos.y)) - ((miny.pos.x - maxy.pos.x) * (midy.pos.y - maxy.pos.y))
+    private val oneOverDY = oneOverDX.unaryMinus()
+    private val dColour =
+        ((colour[1] - colour[2]) * (miny.pos.y - maxy.pos.y)) - ((colour[0] - colour[2]) * (midy.pos.y - maxy.pos.y))
+
+    private val colourXStep = dColour / oneOverDX
+    private val colourYStep = dColour / oneOverDY
+
+}
