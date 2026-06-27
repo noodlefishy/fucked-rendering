f8a1ecb8ba029e9ff0afc61a14ce9015a33754b4
Needy Inky <pikachouuwu2@gmail.com>
2026-06-27 12:45:43 +0200

    many missing bits

--- src/main/kotlin/Edge.kt
+++ src/main/kotlin/Edge.kt
@@ -23,6 +23,23 @@ class Edge(
     var textureCord: Vector4f =
         gradients.textureCords[minyIndex] + (gradients.textureCordYStep * yPreStep) + (gradients.textureCordXStep * xPreStep)
 
+    val textureCordX = gradients.textureCords[minyIndex].x +
+            gradients.textureCordXStep.x * xPreStep +
+            gradients.textureCordXStep.y * yPreStep
+
+    val textureCordXStep = gradients.textureCordXStep.y + gradients.textureCordXStep.x * xStep
+
+
+    val textureCordY = gradients.textureCords[minyIndex].y +
+            gradients.textureCordYStep.x * xPreStep +
+            gradients.textureCordYStep.y * yPreStep
+
+    val textureCordYStep = gradients.textureCordYStep.y + gradients.textureCordYStep.x * xStep
+
+
+//    val oneOverZStep =
+
+
     val colourStep: Vector4f = gradients.textureCordYStep + (gradients.textureCordXStep * xStep)
     fun step() {
         x += xStep
